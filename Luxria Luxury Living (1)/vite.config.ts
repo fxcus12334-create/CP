7b33a564a949f174d57920a16704cd7b88143a59
fxcus12334-create <fxcus12334@gmail.com>
2026-05-27 21:19:38 +0400

    Update vite.config.ts

--- Luxria Luxury Living (1)/vite.config.ts
+++ Luxria Luxury Living (1)/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { tanstackStartVite } from "@tanstack/react-start/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [
-    tanstackStartVite(),
+    TanStackRouterVite(),
+    react(),
     tsconfigPaths()
   ]
 });
